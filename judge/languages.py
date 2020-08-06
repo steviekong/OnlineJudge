b3b59552a95f44ee985529d0d808128442698474
steviekong <sidharth.reji@gmail.com>
2020-08-06 16:43:43 +0530

    fixed nodejs verison

--- judge/languages.py
+++ judge/languages.py
@@ -223,5 +223,5 @@ languages = [
     {"config": _py2_lang_config, "name": "Python2", "description": "Python 2.7", "content_type": "text/x-python"},
     {"config": _py3_lang_config, "name": "Python3", "description": "Python 3.6", "content_type": "text/x-python"},
     {"config": _go_lang_config, "name": "Golang", "description": "Golang 1.14", "content_type": "text/x-go"},
-    {"config": _js_lang_config, "name": "JavaScript", "description": "JavaScript ES6", "content_type": "text/javascript"}
+    {"config": _js_lang_config, "name": "JavaScript", "description": "Node.js 14.7.0", "content_type": "text/javascript"}
 ]
