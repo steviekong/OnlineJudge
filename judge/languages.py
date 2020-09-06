d518680b03adfb02386838230243a98fd08b2dcc
steviekong <sidharth.reji@gmail.com>
2020-09-06 17:01:13 +0530

    fixed js stack size

--- judge/languages.py
+++ judge/languages.py
@@ -209,7 +209,7 @@ _js_lang_config = {
 //APPEND END""",
     "run": {
         "exe_name": "solution.js",
-        "command": "/usr/bin/nodejs {exe_path} --stack-size=2500",
+        "command": "/usr/bin/nodejs --stack-size=10000 {exe_path}",
         "seccomp_rule": None,
         "env": default_env
     }
